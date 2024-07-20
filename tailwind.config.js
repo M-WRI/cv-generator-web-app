ba404add576534a162070c9772598549f07f8547
Moritz Wright <moritzwright@MBP-von-Moritz.fritz.box>
2024-07-20 20:19:10 +0200

    tailwind base styles

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        primary: "#1800E9",
+        error: "#F44336",
+      },
+      fontSize: {
+        xs: "12px",
+        sm: "16px",
+        base: "18px",
+        l: "24px",
+        xl: "36px",
+        "2xl": "48px",
+        "3xl": "60px",
+      },
+      fontFamily: {
+        primary: ['"Space Mono"', "monospace"],
+        secondary: ['"Montserrat"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
