9a8d533ee8c4871e9bf33cdb281dbc5d5f13b952
M-WRI <68390629+M-WRI@users.noreply.github.com>
2024-07-23 20:20:12 +0200

    dashboard has right behavior in case of mobile (#11)
    Co-authored-by: Moritz Wright <moritzwright@MBP-von-Moritz.fritz.box>

--- src/components/screens/dashboard/Dashboard.tsx
+++ src/components/screens/dashboard/Dashboard.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Accordion, CVAccordionContent, Header } from "../../organisms";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export const Dashboard: React.FC = () => {
+  const location = useLocation();
   const accordionItems = [
     {
       id: 1,
@@ -11,14 +12,20 @@ export const Dashboard: React.FC = () => {
     },
   ];
 
+  const showRightSide = location.pathname !== "/dashboard";
+
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col-reverse sm:flex-col h-screen">
       <Header />
-      <div className="flex-grow grid grid-cols-2 gap-4">
-        <aside className="list-none p-0">
+      <div className="flex-grow grid grid-cols-1 md:grid-cols-2 gap-4">
+        <aside
+          className={`list-none p-0 ${
+            showRightSide ? "hidden md:block" : "block"
+          }`}
+        >
           <Accordion accordionItems={accordionItems} />
         </aside>
-        <main>
+        <main className={`${showRightSide ? "block" : "hidden"} md:block`}>
           <Outlet />
         </main>
       </div>
